efcac069b77603dfbf1bf4c106e0a006ef86bf63
siewla <siewlapang@gmail.com>
2021-04-22 13:55:04 +0800

    add templates

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    # form = BookForm()
+    # if request.method == 'POST':
+    #     form = BookForm(request.POST, request.FILES)
+    #     if form.is_valid():
+    #         form.save()
+    #         return redirect('books_index')
+    # books = Book.objects.all()
+    # context = {"form": form, "books": books}
+    return render(request, 'bookstore/index.html')
