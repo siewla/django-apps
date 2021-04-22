6deddb88dc674f7bbb6d4c6916e9c6986f34ffda
siewla <siewlapang@gmail.com>
2021-04-22 14:15:19 +0800

    add index to installed app, must do

--- apps/settings.py
+++ apps/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['django-az.herokuapp.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'index',
     'bookstore',
     'django.contrib.admin',
     'django.contrib.auth',
